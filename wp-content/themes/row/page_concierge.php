c47763d60fd769d097e826ed327e8c59e06b4544
Mandino <dino@misfit-inc.com>
2014-02-27 22:43:02 +0800

    Fixed concierge page

--- wp-content/themes/row/page_concierge.php
+++ wp-content/themes/row/page_concierge.php
@@ -44,10 +44,13 @@
 		
 			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>	
 			
-			
-			<h1><?php the_title(); ?></h1>
+			<div class="concierge-top-wrapper">
 
-			<?php the_content(); ?>
+				<h1><?php the_title(); ?></h1>
+
+				<?php the_content(); ?>
+
+			</div>
 			
 			
 			<?php endwhile; endif; wp_reset_query(); ?>	
