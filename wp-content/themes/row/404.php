1b1849a29959732188b47b9a63be8e54c9ec28ba
Mandino Cheng <dino@misfit-inc.com>
2014-03-11 11:03:28 +0800

    Created 404 page

--- wp-content/themes/row/404.php
+++ wp-content/themes/row/404.php
@@ -11,28 +11,27 @@
 <?php include (TEMPLATEPATH . '/library/navigation.php'); ?>
 
 </div>
+
+<section class="contentarea">
+
+	<div class="home-intro">
+									
+		<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"></div>
+			
+	</div>	
 	
 	<!-- Start Basic Page -->	
-	<div id="basicpage">
-	
-		<div class="container">
-		
-			<div class="boxer">
+	<div class="page-content alt-page-content" style="padding: 30px;">
 			
-				<h1><?php _e('Oh No&hellip;','cebolang'); ?></h1>
-				
-				<span class="fourohfour"><?php _e('404','cebolang'); ?></span>
-				
-				<p><?php _e('The page you are looking for is not available. Please try your search again', 'cebolang'); ?></p>
-				<p><a class="button fuschia second" href="#modal"><?php _e('Contact Us','cebolang'); ?></a> <a class="button"href="http://twitter.com/<?php echo get_option('cebo_twitter'); ?>" target="_blank"><?php _e('Follow Us On Twitter','cebolang'); ?></a></p></p>
-				
-							
-			</div>			
-						
-		</div>
+		<h1><?php _e('Oh No&hellip;','cebolang'); ?></h1>
 		
+		<span class="fourohfour"><?php _e('404','cebolang'); ?></span>
+		
+		<p><?php _e('The page you are looking for is not available. Please try your search again', 'cebolang'); ?></p>
+		<p><a class="button fuschia second" href="/contact"><?php _e('Contact Us','cebolang'); ?></a> <a class="button"href="http://twitter.com/<?php echo get_option('cebo_twitter'); ?>" target="_blank"><?php _e('Follow Us On Twitter','cebolang'); ?></a></p></p>
+
 		<div class="clear"></div>
-		
+						
 	</div>
 	<!-- End Basic Page -->
 	
