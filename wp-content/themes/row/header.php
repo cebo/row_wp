b5cda63710779ca69c260090955bf3374474e086
Mandino <dino@misfit-inc.com>
2014-04-28 22:55:41 +0800

    Eat & Drink Logic code integration with WPML

--- wp-content/themes/row/header.php
+++ wp-content/themes/row/header.php
@@ -664,37 +664,117 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 					
 					
 						<ul class="dropbox">
-					
+						
+						<?php 
+
+							global $sitepress;
+							// save current language
+							$current_lang = $sitepress->get_current_language();
+							//get the default language
+							$default_lang = $sitepress->get_default_language();
+							//fetch posts in default language
+							$sitepress->switch_lang($default_lang);
+							//query args
+							$custom_query_args_two = array(
+							    'post_type' => 'page', 
+							    'page_id' => 54, 
+							);
+
+						?>
+
+
 						<li class="drop-intro">
+
+							<?php
+								//build query
+								$custom_query_two = new wp_query($custom_query_args_two);
+								//loop
+								while ( $custom_query_two->have_posts() ) : $custom_query_two->the_post();
+								    //check if a translation exist
+								    $t_post_id_two = icl_object_id($post->ID, 'post', false, $current_lang);
+								    if(!is_null($t_post_id_two)){
+								        $t_post = get_post( $t_post_id_two);
+						    ?>
+
+								<h1><?php the_title($t_post_id_two); ?></h1>
 							
-							<?php query_posts('post_type=page&p=54&suppress_filters=1'); if(have_posts()) : while(have_posts()) : the_post(); ?>
-								
+								<p><?php echo excerpt(13); ?></p>
+
+							<?php } else {  //no translation? display default language ?>							
+
+								<?php //query_posts('post_type=page&p=54&suppress_filters=0'); if(have_posts()) : while(have_posts()) : the_post(); ?>
+																
 								<h1><?php the_title(); ?></h1>
 							
 								<p><?php echo excerpt(13); ?></p>
+																							
+							<?php } endwhile; wp_reset_query(); ?>	
+
+						</li>
+
+						<?php 
+
+							//query args
+							$custom_query_args = array(
+							    'post_type' => 'amenities', 
+							    'posts_per_page' => 3, 
+							    'post__not_in' => array(32,33), 
+							);
+
+							//build query
+							$custom_query = new wp_query($custom_query_args);
+							//loop
+							while ( $custom_query->have_posts() ) : $custom_query->the_post();
+							    //check if a translation exist
+							    $t_post_id = icl_object_id($post->ID, 'post', false, $current_lang);
+							    if(!is_null($t_post_id)){
+							        $t_post = get_post( $t_post_id);
+					    ?>
+
+
+					        <li>
+					
+								<?php if(get_post_meta($t_post_id, 'cebo_thumbtwo', true)) { ?>
+							
+								<a href="<?php the_permalink($t_post_id); ?>"><img src="<?php echo get_post_meta($t_post_id, 'cebo_thumbtwo', true); ?>" alt="<?php the_title($t_post_id); ?>"></a>
 								
-							<?php endwhile; endif; wp_reset_query(); ?>								
-						</li>
-						
-						<?php query_posts(array('post_type' => 'amenities', 'posts_per_page' => 3, 'post__not_in' => icl_object_id(array(32,33), true), 'suppress_filters' => 1,)); if(have_posts()) : while(have_posts()) : the_post(); 	$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>						
-						
-						<li>
-							
-							<?php if(get_post_meta($post->ID, 'cebo_thumbtwo', true)) { ?>
-							
-							<a href="<?php the_permalink(); ?>"><img src="<?php echo get_post_meta($post->ID, 'cebo_thumbtwo', true); ?>" alt="<?php the_title(); ?>"></a>
-							
-							<?php } elseif($imgsrc) { ?>
+								<?php } elseif($imgsrc) { ?>
 
-													
-							<a href="<?php the_permalink(); ?>"><img src="<?php echo $imgsrc[0]; ?>" alt="<?php the_title(); ?>"></a>
+														
+								<a href="<?php the_permalink($t_post_id); ?>"><img src="<?php echo $imgsrc[0]; ?>" alt="<?php the_title($t_post_id); ?>"></a>
 
-							<?php } ?>			
+								<?php } ?>			
 
-							<h3><?php the_title(); ?></h3>
-						</li>
-						
-						<?php endwhile; endif; wp_reset_query(); ?>	
+								<h3><?php the_title($t_post_id); ?></h3>
+								
+							</li>		
+
+				        <?php } else {  //no translation? display default language ?>
+
+					        <li>
+							
+								<?php if(get_post_meta($post->ID, 'cebo_thumbtwo', true)) { ?>
+							
+								<a href="<?php the_permalink(); ?>"><img src="<?php echo get_post_meta($post->ID, 'cebo_thumbtwo', true); ?>" alt="<?php the_title(); ?>"></a>
+								
+								<?php } elseif($imgsrc) { ?>
+
+														
+								<a href="<?php the_permalink(); ?>"><img src="<?php echo $imgsrc[0]; ?>" alt="<?php the_title(); ?>"></a>
+
+								<?php } ?>			
+
+								<h3><?php the_title(); ?></h3>
+
+							</li>							        
+
+						<?php
+							
+							} endwhile; wp_reset_query(); //$sitepress->switch_lang($current_lang);
+
+							//query_posts(array('post_type' => 'amenities', 'posts_per_page' => 3, 'post__not_in' => icl_object_id(array(32,33), true), 'suppress_filters' => 0,)); if(have_posts()) : while(have_posts()) : the_post(); 	$imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); 
+
+						?>
 						
 												
 					</ul>
