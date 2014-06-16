4bf78e3c54c1309a438c3d462abb244048be7672
Cebo Campbell <cebo@cebocampbell.com>
2014-06-16 13:51:41 -0500

    Small Fixes to blog

--- wp-content/themes/row/sidebar.php
+++ wp-content/themes/row/sidebar.php
@@ -11,77 +11,82 @@
 
    <!-- widgetized  -->
 
-			<?php if ( !function_exists('dynamic_sidebar')
-				|| !dynamic_sidebar('Sidebar') ) : ?>
-		<?php endif; ?>  
+	<?php 
+
+		if( !is_page_template( 'page_blog.php' ) ) { 
+
+			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) :	endif; 
+
+		}
+	?>  
 	
-		<!-- widgetized  -->	
+	<!-- widgetized  -->					
+				
+	<?php if(get_option('cebo_ad1code-removethistobringback')) { ?>
+	
+		<?php echo get_option('cebo_ad1code'); ?>
+	
+	<?php } elseif (get_option('cebo_ad1image-removethistobringback')) { ?>
+	
+		<a href="<?php echo get_option('cebo_ad1link'); ?>" target="_blank"><img style="border-bottom: 1px dashed #EEEEEE; padding-bottom: 7px;" src="<?php echo get_option('cebo_ad1image'); ?>" alt="" /></a>
+
+	<?php } ?>
+		
+		
+		<div class="populararticles">
+			<h2 class="sidehead">Popular This Week</h2>
+			
+			<ul>
+			
+			<?php query_posts('post_type=post&posts_per_page=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+				<li>
 				
 				
-				<?php if(get_option('cebo_ad1code')) { ?>
-				
-				<?php echo get_option('cebo_ad1code'); ?>
-				
-				<?php } elseif (get_option('cebo_ad1image')) { ?>
-				
-				<a href="<?php echo get_option('cebo_ad1link'); ?>" target="_blank"><img src="<?php echo get_option('cebo_ad1image'); ?>" alt="" /></a>
-				<?php } ?>
 					
+					<span class="popimg"  style="background-image:url(<?php echo $imgsrc[0]; ?>);">
+						<a href="<?php the_permalink(); ?>"></a>
+					</span>
 					
-					<div class="populararticles">
-						<h2 class="sidehead">Popular This Week</h2>
-						
-						<ul>
-						
-						<?php query_posts('post_type=post&posts_per_page=5'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-							<li>
-							
-							
-								
-								<span class="popimg"  style="background-image:url(<?php echo $imgsrc[0]; ?>);">
-									<a href="<?php the_permalink(); ?>"></a>
-								</span>
-								
-								<a href="<?php the_permalink(); ?>" class="poptitle"><?php the_title(); ?></a>
-								
-																<?php
-							$category = get_the_category(); 
-							
-							?>
-							<a class="popcat" href="<?php echo get_category_link($category[0]->term_id ); ?>"><?php echo $category[0]->cat_name; ?></a>
-								
-							</li>
-							
-							
-							<?php endwhile; endif; wp_reset_query(); ?>	
-							
-														
-							
-						</ul>
-					</div><!-- end popular items -->
+					<a href="<?php the_permalink(); ?>" class="poptitle"><?php the_title(); ?></a>
 					
-					
-					<?php if(get_option('cebo_ad2code')) { ?>
+													<?php
+				$category = get_the_category(); 
 				
-				<?php echo get_option('cebo_ad2code'); ?>
-				
-				<?php } elseif (get_option('cebo_ad2image')) { ?>
-				
-				<a href="<?php echo get_option('cebo_ad2link'); ?>" target="_blank"><img src="<?php echo get_option('cebo_ad2image'); ?>" alt="" /></a>
-				<?php } ?>
+				?>
+				<a class="popcat" href="<?php echo get_category_link($category[0]->term_id ); ?>"><?php echo $category[0]->cat_name; ?></a>
 					
-					
-										
-					<div class="instas">
-						<h2 class="sidehead">Stay connected. Hashtag us!<span>#MoreNYthanNY</span></h2>
-						
-						<div id="instafeed"></div>
-						
-					</div>
+				</li>
+				
+				
+				<?php endwhile; endif; wp_reset_query(); ?>	
+				
+											
+				
+			</ul>
+		</div><!-- end popular items -->
+		
+		
+		<?php if(get_option('cebo_ad2code-removethistobringback')) { ?>
+	
+	<?php echo get_option('cebo_ad2code'); ?>
+	
+	<?php } elseif (get_option('cebo_ad2image-removethistobringback')) { ?>
+	
+	<a href="<?php echo get_option('cebo_ad2link'); ?>" target="_blank"><img src="<?php echo get_option('cebo_ad2image'); ?>" alt="" /></a>
+	<?php } ?>
+		
+		
+							
+		<div class="instas">
+			<h2 class="sidehead">Stay connected. Hashtag us!<span>#MoreNYthanNY</span></h2>
+			
+			<div id="instafeed"></div>
+			
+		</div>
 
 
-				
-				</div>
+	
+	</div>
 			
 				
 <!-- end this sidebar -->		
