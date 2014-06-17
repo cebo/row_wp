3589def11246c6b5f0a9ee5c5cedb2a4b85b7150
Mandino <dino@misfit-inc.com>
2014-06-17 18:32:52 +0800

    add shoot share smile to amenities page

--- wp-content/themes/row/page_amenities.php
+++ wp-content/themes/row/page_amenities.php
@@ -208,11 +208,43 @@
 
 			</div>
 
+
+
+			<?php query_posts('post_type=amenities&p=3097&suppress_filters=1'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
 			
-			<div class="clear"></div>
+				<div class="fourth-level">
+				
+					<div class="fullspan">
+					
+						<div class="suboverlay narrow">
+						
+							<h1><?php the_title(); ?></h1>
+				
+							<p><?php echo excerpt(20); ?>. <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
+										
+						</div>
+						
+						<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
+									
+							<div class="stretch"  style="background-image: url(<?php echo get_post_meta($post->ID, 'cebo_fullpic', true); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } elseif($imgsrc) { ?>
+							
+							
+							<div class="stretch"  style="background-image: url(<?php echo $imgsrc[0]; ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } else { ?>
+												
+							<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						<?php } ?>
 
-			</div>
+					</div>					
 
-
+				</div>
 			
+			<?php endwhile; endif; wp_reset_query(); ?>				
+			
+
+
 <?php get_footer(); ?>
