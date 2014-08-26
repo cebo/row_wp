79078d28b0268913136e4d5662814d5709e7e725
Mandino <dino@misfit-inc.com>
2014-08-26 12:03:25 +0800

    boscobox testing

--- wp-content/themes/row/single-amenities.php
+++ wp-content/themes/row/single-amenities.php
@@ -13,52 +13,50 @@
 
 	<div class="home-intro">
 	
-		<?php if(get_post_meta($post->ID, 'cebo_fullbannerpic', true)) { ?>
+		<?php if(get_post_meta($post->ID, 'cebo_pulled_photos', true)) { ?>
+
+			<?php
+
+				include(TEMPLATEPATH . '/library/dropbox-utils.php'); 
+
+				/** settings **/
+				$images_dir = 'https://www.dropbox.com/sh/3coxz0uzkhfi9ej/AABpY7uKqCd_kGJWA0tzLYITa';
+				$thumbs_dir = bloginfo('template_url') . '/images/bosco-box/';
+				$thumbs_width = 200;
+				$images_per_row = 3;
+
+				
+				/** generate photo gallery **/
+				$image_files = get_files($images_dir);
+				if(count($image_files)) {
+				  $index = 0;
+				  foreach($image_files as $index=>$file) {
+				    $index++;
+				    $thumbnail_image = $thumbs_dir.$file;
+				    if(!file_exists($thumbnail_image)) {
+				      $extension = get_file_extension($thumbnail_image);
+				      if($extension) {
+				        make_thumb($images_dir.$file,$thumbnail_image,$thumbs_width);
+				      }
+				    }
+				    echo '<a href="',$images_dir.$file,'" class="photo-link smoothbox" rel="gallery"><img src="',$thumbnail_image,'" /></a>';
+				    if($index % $images_per_row == 0) { echo '<div class="clear"></div>'; }
+				  }
+				  echo '<div class="clear"></div>';
+				}
+				else {
+				  echo '<p>There are no images in this gallery.</p>';
+				}
+
+			?>
+
+		<?php } elseif(get_post_meta($post->ID, 'cebo_fullbannerpic', true)) { ?>
 								
 			<div class="stretch"  style="background-image: url(<?php echo get_post_meta($post->ID, 'cebo_fullbannerpic', true); ?>);"></div>
 		
 		<?php } elseif(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
 										
-			<div class="stretch"  style="background-image: url(<?php echo get_post_meta($post->ID, 'cebo_fullpic', true); ?>);"></div>
-
-		<?php } elseif(get_post_meta($post->ID, 'cebo_pulled_photos', true)) { ?>
-
-			<?php
-
-				/** settings **/
-				$images_dir = '';
-				$thumbs_dir = '';
-				$thumbs_width = 200;
-				$images_per_row = 3;
-
-				/** generate photo gallery **/
-				$image_files = get_files($images_dir);
-				
-				if(count($image_files)) {
-				  $index = 0;
-
-				 //  foreach($image_files as $index=>$file) {
-
-					//     $index++;
-					//     $thumbnail_image = $thumbs_dir.$file;
-					//     if(!file_exists($thumbnail_image)) {
-					//       $extension = get_file_extension($thumbnail_image);
-					//       if($extension) {
-					//         make_thumb($images_dir.$file,$thumbnail_image,$thumbs_width);
-					//       }
-					//     }
-
-					//     echo $thumbnail_image; 
-
-					// }
-
-				 ?>
-
-			  		<div class="stretch"  style="background-image: url(<?php echo $thumbnail_image[0]; ?>);"></div>
-
-				<?php } ?>
-								
-			
+			<div class="stretch"  style="background-image: url(<?php echo get_post_meta($post->ID, 'cebo_fullpic', true); ?>);"></div>			
 		
 		<?php } elseif($imgsrc) { ?>
 		
