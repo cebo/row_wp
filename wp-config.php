ed0a6a8b91e785fd2523b1ec37099d6ca5560401
Cebo <cebo@cebocampbell.com>
2015-04-24 09:04:19 -0500

    A few fixes to the booking calendar
    booking cal fixes

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@ else {
  
 	// ** MySQL settings - You can get this info from your web host ** //
 	/** The name of the database for WordPress */
-	define('DB_NAME', 'row');
+	define('DB_NAME', 'newrow');
 
 	/** MySQL database username */
 	define('DB_USER', 'root');
 
 	/** MySQL database password */
-	define('DB_PASSWORD', '');
+	define('DB_PASSWORD', 'root');
 
 	/** MySQL hostname */
 	define('DB_HOST', 'localhost');
@@ -38,8 +38,8 @@ else {
 	/** The Database Collate type. Don't change this if in doubt. */
 	define('DB_COLLATE', '');
 
-	define('WP_HOME','http://localhost/row');
-	define('WP_SITEURL','http://localhost/row');
+	define('WP_HOME','http://www.rownyc-dk.dev');
+	define('WP_SITEURL','http://www.rownyc-dk.dev');
 
 }
 
