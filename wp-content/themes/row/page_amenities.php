86e35b057daa2fb3aa0da05dc351ff9b96b94134
sphrcl <sphericalcommunications@gmail.com>
2015-06-08 21:18:05 -0400

    reordering amenities

--- wp-content/themes/row/page_amenities.php
+++ wp-content/themes/row/page_amenities.php
@@ -21,60 +21,8 @@
 		
 		</div>		
 
-			<!-- begin fourth level -->
-			
-			
-			
-			
-			<?php query_posts('post_type=amenities&p=30'); if(have_posts()) : while(have_posts()) : the_post(); ?>
-			
-			
-			<div class="fourth-level">
-			
-			
-				<div class="fullspan" style="">
-				
-					<div class="suboverlay narrow">
-					
-			
-					
-						<h1><?php the_title(); ?></h1>
-						
-			
-							<p><?php echo excerpt(18); ?>. <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
-							
-							
-						</div>
-					
-					<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
-								
-								
-						<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_listimage', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-						
-						<?php } elseif($imgsrc) { ?>
-						
-						
-						<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-						
-						<?php } else { ?>
-											
-						<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-						
-						
-					
-					<?php } ?>
-					
-
-				</div>
-				
-
-			</div>
-			
-			<?php endwhile; endif; wp_reset_query(); ?>	
 			<?php query_posts('post_type=amenities&p=31'); if(have_posts()) : while(have_posts()) : the_post(); ?>
 			
-			
-
 			<div class="fourth-level">
 			
 			
@@ -114,8 +62,54 @@
 			</div>
 
 			<?php endwhile; endif; wp_reset_query(); ?>	
-			
 
+			<!-- begin fourth level -->
+			
+			<?php query_posts('post_type=amenities&p=30'); if(have_posts()) : while(have_posts()) : the_post(); ?>
+			
+			
+			<div class="fourth-level">
+			
+			
+				<div class="fullspan" style="">
+				
+					<div class="suboverlay narrow">
+					
+			
+					
+						<h1><?php the_title(); ?></h1>
+						
+			
+							<p><?php echo excerpt(18); ?> <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
+							
+							
+						</div>
+					
+					<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
+								
+								
+						<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_listimage', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						<?php } elseif($imgsrc) { ?>
+						
+						
+						<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						<?php } else { ?>
+											
+						<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						
+					
+					<?php } ?>
+					
+
+				</div>
+				
+
+			</div>
+			
+			<?php endwhile; endif; wp_reset_query(); ?>	
 
 			<div class="third-level">
 			
@@ -211,78 +205,7 @@
 
 
 
-			<?php query_posts('post_type=amenities&p=3097'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-			
-				<div class="fourth-level">
-				
-					<div class="fullspan">
-					
-						<div class="suboverlay narrow">
-						
-							<h1><?php the_title(); ?></h1>
-				
-							<p><?php echo get_post_meta($post->ID, 'cebo_amenitiesblurb', true); ?><br />
-							 <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
-										
-						</div>
-						
-						<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
-									
-							<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_fullpic', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-							
-							<?php } elseif($imgsrc) { ?>
-							
-							
-							<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-							
-							<?php } else { ?>
-												
-							<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-						
-						<?php } ?>
 
-					</div>					
-
-				</div>
-			
-			<?php endwhile; endif; wp_reset_query(); ?>	
-
-			<!-- fitness center -->
-			<?php query_posts('post_type=amenities&p=5819'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
-			
-				<div class="fourth-level">
-				
-					<div class="fullspan">
-					
-						<div class="suboverlay narrow">
-						
-							<h1><?php the_title(); ?></h1>
-				
-							<p><?php echo excerpt(15); ?>. <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
-										
-						</div>
-						
-						<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
-									
-							<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_fullpic', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-							
-							<?php } elseif($imgsrc) { ?>
-							
-							
-							<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-							
-							<?php } else { ?>
-												
-							<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
-						
-						<?php } ?>
-
-					</div>					
-
-				</div>
-			
-			<?php endwhile; endif; wp_reset_query(); ?>
-			<!-- end fitness center -->			
 
 			<!-- Cyc Fitness -->
 			<?php query_posts('post_type=amenities&p=6485'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
@@ -356,6 +279,77 @@
 				</div>
 			
 			<?php endwhile; endif; wp_reset_query(); ?>
-			<!-- end fitness center -->	
+			<!-- end fitness center -->
+			<?php query_posts('post_type=amenities&p=3097'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+			
+				<div class="fourth-level">
+				
+					<div class="fullspan">
+					
+						<div class="suboverlay narrow">
+						
+							<h1><?php the_title(); ?></h1>
+				
+							<p><?php echo get_post_meta($post->ID, 'cebo_amenitiesblurb', true); ?><br />
+							 <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
+										
+						</div>
+						
+						<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
+									
+							<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_fullpic', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } elseif($imgsrc) { ?>
+							
+							
+							<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } else { ?>
+												
+							<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						<?php } ?>
+
+					</div>					
+
+				</div>
+			
+			<?php endwhile; endif; wp_reset_query(); ?>				
+			<!-- fitness center -->
+			<?php query_posts('post_type=amenities&p=5819'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
+			
+				<div class="fourth-level">
+				
+					<div class="fullspan">
+					
+						<div class="suboverlay narrow">
+						
+							<h1><?php the_title(); ?></h1>
+				
+							<p><?php echo excerpt(15); ?>. <a href="<?php the_permalink(); ?>"><?php _e('MORE >','row-theme-text'); ?></a></p>
+										
+						</div>
+						
+						<?php if(get_post_meta($post->ID, 'cebo_fullpic', true)) { ?>
+									
+							<div class="stretch"  style="background-image: url(<?php echo tt(get_post_meta($post->ID, 'cebo_fullpic', true),1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } elseif($imgsrc) { ?>
+							
+							
+							<div class="stretch"  style="background-image: url(<?php echo tt($imgsrc[0],1123,320); ?>);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+							
+							<?php } else { ?>
+												
+							<div class="stretch"  style="background-image: url(<?php bloginfo ('template_url'); ?>/images/watermark.jpg);"><a href="<?php the_permalink(); ?>" style="height: 100%; width: 100%;"></a></div>
+						
+						<?php } ?>
+
+					</div>					
+
+				</div>
+			
+			<?php endwhile; endif; wp_reset_query(); ?>
+			<!-- end fitness center -->					
 
 <?php get_footer(); ?>
