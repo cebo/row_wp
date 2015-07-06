bac5aa51e342bbe87644063b157a9216c7d38041
Jhelline <jhelline.fausto@outsourced.ph>
2015-07-06 22:41:05 +0800

    sojern ixels

--- wp-content/themes/row/footer.php
+++ wp-content/themes/row/footer.php
@@ -129,19 +129,19 @@
 
 			// Please do not modify the below code.
 			var cid = [];
-			var version = '3';
-			var paramsArr = ['v=' + version];
-			var cidParams = ["hconfno","hp","hd1","hd2","hpid","hb","hpr","hdc"];
-			var pl = document.createElement('script');
-			var defaultParams = {"vid":"hot","et":"hpr"};
-			for(key in defaultParams) { params[key] = defaultParams[key]; };
-			for(key in cidParams) { cid.push(params[cidParams[key]]); };
-			params.cid = cid.join('|');
-			for(key in params) { paramsArr.push(key + '=' + encodeURIComponent(params[key])) };
-			pl.type = 'text/javascript';
-			pl.async = true;
-			pl.src = 'https://beacon.sojern.com/pixel/p/5925?' + paramsArr.join('&');
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
+		    var version = '3';
+		    var paramsArr = ['v=' + version];
+		    var cidParams = ["hconfno","hp","hd1","hd2","hpid","hb","hpr","hdc"];
+		    var pl = document.createElement('script');
+		    var defaultParams = {"vid":"hot","et":"hpr"};
+		    for(key in defaultParams) { params[key] = defaultParams[key]; };
+		    for(key in cidParams) { cid.push(params[cidParams[key]]); };
+		    params.cid = cid.join('|');
+		    for(key in params) { paramsArr.push(key + '=' + encodeURIComponent(params[key])) };
+		    pl.type = 'text/javascript';
+		    pl.async = true;
+		    pl.src = 'https://beacon.sojern.com/pixel/p/6042?' + paramsArr.join('&');
+		    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
 		})();
 	</script>
 	<!-- End Sojern Tag -->
@@ -153,29 +153,23 @@
 		    var params = {
 		       pname: "<?php the_title(); ?>", // Page Name
 		       domain: "http://rownyc.com", // Site Domain
-		       pc: "<?php
-						if (has_category()) {
-						    $category = get_the_category();
-						    echo $category[0]->cat_name;
-						} else { echo 'uncategorized'; }
-					?>" // Page Category
 		    };
 
 			// Please do not modify the below code.
 			var cid = [];
-			var version = '3';
-			var paramsArr = ['v=' + version];
-			var cidParams = ["hconfno","hp","hd1","hd2","hpid","hb","hpr","hdc"];
-			var pl = document.createElement('script');
-			var defaultParams = {"vid":"hot"};
-			for(key in defaultParams) { params[key] = defaultParams[key]; };
-			for(key in cidParams) { cid.push(params[cidParams[key]]); };
-			params.cid = cid.join('|');
-			for(key in params) { paramsArr.push(key + '=' + encodeURIComponent(params[key])) };
-			pl.type = 'text/javascript';
-			pl.async = true;
-			pl.src = 'https://beacon.sojern.com/pixel/p/5923?' + paramsArr.join('&');
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
+		    var version = '3';
+		    var paramsArr = ['v=' + version];
+		    var cidParams = ["hconfno","hp","hd1","hd2","hpid","hb","hpr","hdc"];
+		    var pl = document.createElement('script');
+		    var defaultParams = {"vid":"hot"};
+		    for(key in defaultParams) { params[key] = defaultParams[key]; };
+		    for(key in cidParams) { cid.push(params[cidParams[key]]); };
+		    params.cid = cid.join('|');
+		    for(key in params) { paramsArr.push(key + '=' + encodeURIComponent(params[key])) };
+		    pl.type = 'text/javascript';
+		    pl.async = true;
+		    pl.src = 'https://beacon.sojern.com/pixel/p/6043?' + paramsArr.join('&');
+		    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
 		})();
 	</script>
 	<!-- End Sojern Tag -->
