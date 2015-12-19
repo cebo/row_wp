7a077e0ab900d20295c1d83effa6c21579644f7d
Mandino <dino@misfit-inc.com>
2015-12-19 21:03:45 +0800

    fix: header redirect for mobile without manual redirects

--- wp-content/themes/row/header.php
+++ wp-content/themes/row/header.php
@@ -22,12 +22,21 @@
 		}
 
 		$check = $detect->isMobile(); 
+		$parse = parse_url($_SERVER['REQUEST_URI']);
+		//print_r($parse['path']);
+		
+		print_r('http://m.rownyc.com' . $parse['path']);
+		if( $check && !get_post_meta($post->ID,'cebo_redirect_url', true) ) {
+			header("HTTP/1.1 301 Moved Permanently");
+			header( 'Location: http://m.rownyc.com' . $parse['path'] );
+			exit();
+		} 
 
 		if( $check && get_post_meta($post->ID,'cebo_redirect_url', true) ) {
 			header("HTTP/1.1 301 Moved Permanently");
-			header( 'Location: ' .  get_post_meta($post->ID,'cebo_redirect_url', true) );
+			header( 'Location: ' . get_post_meta($post->ID,'cebo_redirect_url', true) );
 			exit();
-		} 
+		}
 
 		elseif( $check ) { redirect(); }
 
